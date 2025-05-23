0d903ff893859765068bfd8a06bb2f5081863a23
tot-ina27 <anto003.napolano@gmail.com>
2025-05-23 11:48:14 +0200

    MainFrame
    Aggiunta MainFrame al progetto

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class MainFrame exteds JFrame {
+    public MainFrame(String username){
+        setTitle("ToDo Manager - Bacheche");
+        setSize(1000. 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        //Header
+        JLaber welcomeLabel = newJLabel("Benvenuto," + username);
+        welcomeLabel.setFont(new Font("Segoe UI," Font.BOLD, 18));
+        welcomeLabel.setForeground(Color.WHITE);
+
+        JPanel header = new JPanel(newFlowLayout(FlowLayout.LEFT));
+        header.setBackground(new Color(6, 0, 64)); //#060040
+        header.add(welcomeLabel);
+
+        //Contenuto bacheche
+        JPanel boardsPanel = new JPanel(new GridLayout(1, 3, 10, 10));
+        boardsPanel.setBackground(Color.WHITE);
+        boardsPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        boardsPanel.add(new BachecaPanel("Università"));
+        boardsPanel.add(new BachecaPanel("Lavoro"));
+        boardsPanel.add(new BachecaPanel("Tempo Libero"));
+
+        add(header, BorderLayout.NORTH);
+        add(boardsPanel, BorderLayout.CENTER);
+    }
+}
